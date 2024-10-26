894a9270dcf2ecbc7c8292eb42e208c8f11b679d
Alexis59-design <alex.itten@gmail.com>
2024-10-26 23:09:37 +0200

    rotating red LED

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
 let strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
-strip.showRainbow(1, 360)
+strip.setBrightness(128)
+strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    . # # # .
+    `)
 basic.forever(function () {
-    strip.rotate(1)
+    strip.rotate(-1)
     strip.show()
 })
