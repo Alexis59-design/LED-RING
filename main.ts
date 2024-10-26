c799209dd278cb4ac88f8cac2f7e409a33c76d50
Alexis59-design <alex.itten@gmail.com>
2024-10-26 23:54:09 +0200

    rotating foreward and backward

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 let strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
 strip.setBrightness(128)
-strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+let color = neopixel.colors(NeoPixelColors.Red)
+let speed = 1
+strip.setPixelColor(0, color)
 basic.showLeds(`
     . # # # .
     # . . . #
@@ -9,6 +11,29 @@ basic.showLeds(`
     . # # # .
     `)
 basic.forever(function () {
-    strip.rotate(-1)
-    strip.show()
+    for (let index = 0; index <= 23; index++) {
+        strip.show()
+        strip.rotate(speed)
+        basic.pause(100)
+    }
+    if (color == neopixel.colors(NeoPixelColors.Red)) {
+        color = neopixel.colors(NeoPixelColors.Green)
+        speed = -1
+    } else {
+        if (color == neopixel.colors(NeoPixelColors.Green)) {
+            color = neopixel.colors(NeoPixelColors.Blue)
+            speed = 1
+        } else {
+            if (color == neopixel.colors(NeoPixelColors.Blue)) {
+                color = neopixel.colors(NeoPixelColors.Violet)
+                speed = -1
+            } else {
+                if (color == neopixel.colors(NeoPixelColors.Violet)) {
+                    color = neopixel.colors(NeoPixelColors.Red)
+                    speed = 1
+                }
+            }
+        }
+    }
+    strip.setPixelColor(0, color)
 })
